eb04f08dd51ef16c60641e7415509c5d0fccc664
Central3 <pawarrsumukha@gmail.com>
2025-01-06 22:01:40 +0530

    implement insertion and deletion functionality

--- BST.js
+++ BST.js
@@ -19,7 +19,58 @@ function buildTree(arr) {
 function Tree(arr) {
   arr = [...new Set(arr)].sort((a, b) => a - b);
   const root = buildTree(arr);
-  return root;
+
+  function insert(val) {
+    const newNode = Node(val);
+
+    let parent = null;
+    let curr = root;
+
+    while (curr !== null) {
+      parent = curr;
+      if (val < curr.data) curr = curr.left;
+      else if (val > curr.data) curr = curr.right;
+      else return;
+    }
+
+    if (val < parent.data) parent.left = newNode;
+    else parent.right = newNode;
+  }
+
+  function deleteItem(val) {
+    let curr = root;
+    let parent = null;
+
+    if (curr === null) return;
+
+    while (curr && curr.data !== val) {
+      parent = curr;
+      if (val < curr.data) curr = curr.left;
+      else curr = curr.right;
+    }
+
+    if (curr.left === null || curr.right === null) {
+      let newCurr = curr.left ? curr.left : curr.right;
+
+      if (parent.left === curr) parent.left = newCurr;
+      else parent.right = newCurr;
+    } else {
+      let p = null;
+      let temp = curr.right;
+
+      while (temp.left !== null) {
+        p = temp;
+        temp = temp.left;
+      }
+
+      if (p) p.left = temp.right;
+      else curr.right = temp.right;
+
+      curr.data = temp.data;
+    }
+  }
+
+  return { root, insert, deleteItem };
 }
 
 function preOrder(root) {
@@ -45,5 +96,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const root = Tree(arr);
-// preOrder(root);
-prettyPrint(root);
+root.insert(2);
+root.insert(13);
+root.deleteItem(67);
+prettyPrint(root.root);
